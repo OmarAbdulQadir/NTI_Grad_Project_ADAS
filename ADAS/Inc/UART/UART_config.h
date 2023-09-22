a973d47d88f1d3cb76319dd595e47da1a42950ab
Omar Abdul Qadir <oabdulqadir2@gmail.com>
2023-09-22 11:00:08 +0300

    Edit Header

--- ADAS/Inc/UART/UART_config.h
+++ ADAS/Inc/UART/UART_config.h
@@ -1,9 +1,7 @@
-/*
- * UART_config.h
- *
- *  Created on: Sep 20, 2023
- *      Author: Omar A.Qadir
- */
+/**********************************************************************************/
+/***						Date: 20/9/2023	Day: Wednesday						***/
+/***    UART driver for STM32F401 a graduation project for thE NTI 	program	    ***/
+/**********************************************************************************/
 
 #ifndef UART_CONFIG_H_
 #define UART_CONFIG_H_
