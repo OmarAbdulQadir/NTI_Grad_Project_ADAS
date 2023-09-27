2def87ec983c796845f73d6f9fd9c69b9a49d759
nada <nadaorz070@gmail.com>
2023-09-27 12:45:45 +0300

    Update BSD_program.c

--- ADAS/Src/BSD_program.c
+++ ADAS/Src/BSD_program.c
@@ -4,18 +4,30 @@
 /**********************************************************************************/
 
 /*							Private includes									  */
-#include "../Inc/BSD/BSD_private.h"
+
+#include "../Inc/BDS/BSD_private.h
 #include "../Inc/BSD/BSD_interface.h"
 
 /*--------------------------------------------------------------------------------*/
-/*							Global variables									  */
+/*						Global variables for Ultrasonic Sensors			     	  */
+
+
+extern const u8 Global_u8ReRightUS = 0;
+extern const u8 Global_u8ReLeftUS  = 0;
 
 
 /*--------------------------------------------------------------------------------*/
+/*					Define thresholds for blind spot detection			     	  */
+
+#define BLIND_SPOT_THRESHOLD 50
+
+/*--------------------------------------------------------------------------------*/
 /*					Global functions implementation								  */
+
+
 STD_ReturnType BSD_u8Init( void )
 {
-	return 0;
+	
 }
 
 void BSD_vMain( void )
@@ -23,6 +35,22 @@ void BSD_vMain( void )
 	
 	while( 1 )
 	{
+		 if     ( (Global_u8ReRightUS < BLIND_SPOT_THRESHOLD) &&
+                  (Global_u8ReLeftUS  > BLIND_SPOT_THRESHOLD) ) {
+             // Turn on right LED
+        }
+        else if ( (Global_u8ReRightUS  < BLIND_SPOT_THRESHOLD) &&
+                  (Global_u8ReLeftUS   < BLIND_SPOT_THRESHOLD) ) {
+             // Turn on two LEDs
+        }
+        else if ( (Global_u8ReRightUS > BLIND_SPOT_THRESHOLD) &&
+                  (Global_u8ReLeftUS  < BLIND_SPOT_THRESHOLD) ) {
+             // Turn on left LED
+        }
+
+        else {
+             // Turn off front LEDs
+        }
 		
 	}
 }
