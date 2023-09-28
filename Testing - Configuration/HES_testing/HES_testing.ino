5db7862b538a983ddb751f3ab920a2ca4b822982
Omar Abdul Qadir <oabdulqadir2@gmail.com>
2023-09-28 18:15:37 +0300

    Speed Testing

--- Testing - Configuration/HES_testing/HES_testing.ino
+++ Testing - Configuration/HES_testing/HES_testing.ino
@@ -1,3 +1,4 @@
+int i= 0;
 void setup() {
   pinMode(2, INPUT);
   Serial.begin(9600);
@@ -7,11 +8,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   if(digitalRead(2) == 1)
   {
-    Serial.println(1);
+    Serial.print("Cycle: ");
+    Serial.println(i++);
   }
-  else
-  {
-    Serial.println(0);
-  }
-  delay(500);
 }
