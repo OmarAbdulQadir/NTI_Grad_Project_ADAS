71d635c53efed7752927c823fed4f3cd2c7692f3
nada <nadaorz070@gmail.com>
2023-10-05 14:22:42 +0300

    Update LED_interface.h

--- ADAS_Nucleo/Inc/LED/LED_interface.h
+++ ADAS_Nucleo/Inc/LED/LED_interface.h
@@ -6,6 +6,15 @@
 #ifndef LED_INTERFACE_H_
 #define LED_INTERFACE_H_
 
+// Libraries includes
+#include "../Inc/LIB/STD_TYPES.h"
+#include "../Inc/LIB/BIT_MATH.h"
 
+#define LED_Right_CHANNEL	0
+#define LED_Left_CHANNEL	1
+
+STD_ReturnType LED_u8Init( u8 copy_u8LED_ID );
+STD_ReturnType LED_u8On( u8 copy_u8LED_ID );
+STD_ReturnType LED_u8Off( u8 copy_u8LED_ID );
 
 #endif /* LED_INTERFACE_H_ */
