a2c3d7151f07111a3b654809b20d0eb04c8d00b6
nada <nadaorz070@gmail.com>
2023-10-05 14:22:32 +0300

    Update LED_config.h

--- ADAS_Nucleo/Inc/LED/LED_config.h
+++ ADAS_Nucleo/Inc/LED/LED_config.h
@@ -6,6 +6,18 @@
 #ifndef LED_CONFIG_H_
 #define LED_CONFIG_H_
 
+#include "../LIB/BIT_MATH.h"
+#include "../LIB/STD_TYPES.h"
+#include "../GPIO/GPIO_interface.h"
+#include "LED_private.h"
 
+// Number of channels
+#define LED_NO_CHANNELS		2
+
+// Configurations for LEDs
+#define LedRight_PORTID			GPIOC_PORT
+#define LedRight_PINID			PIN3
+#define LedLeft_PORTID			GPIOC_PORT
+#define LedLeft_PINID			PIN4
 
 #endif /* LED_CONFIG_H_ */
